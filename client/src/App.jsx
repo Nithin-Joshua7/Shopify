a19f75dd44c26c94e565394001a959c625f9073f
Nithin Joshua <nithinjoshua11@gmail.com>
2025-05-04 17:24:38 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -27,7 +27,7 @@ function App() {
       height="100vh"
       justify="center"
       align="center"
-      bg={useColorModeValue("gray.100", "gray.900")}
+     
     >
        <Box bg="red.300" p={4}>Loading...</Box>
     );
